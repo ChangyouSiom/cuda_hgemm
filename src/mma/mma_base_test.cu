52c46f48c01c1e3c52e35f8f0e3990e87c4bba32
changyou <changyou_siom@163.com>
2024-01-11 19:55:45 +0800

    fix error

--- src/mma/mma_base_test.cu
+++ src/mma/mma_base_test.cu
@@ -23,8 +23,8 @@
 #define WARP_TILES_N 8  // WARP_TILES/WARP_TILES_M
 
 __global__ void mmaBaseKernelTest(half *A, half *B, half *C, size_t M, size_t N, size_t K) {
-    int block_tile_i = blockIdx.x;
-    int block_tile_j = blockIdx.y;
+    int block_tile_i = blockIdx.y;
+    int block_tile_j = blockIdx.x;
 
     int thread_id = threadIdx.x;
     int warp_id = thread_id / 32;
@@ -40,16 +40,16 @@ __global__ void mmaBaseKernelTest(half *A, half *B, half *C, size_t M, size_t N,
             int shared_row = warp_id * BLOCK_ROWS / BLOCK_WARPS + lane_id / 4 + i * 8;
             int shared_col = lane_id % 4 * 8;
             int block_row = block_tile_i * BLOCK_ROWS + shared_row;
-            int block_col = k_tile * CHUNK * WARP_K + shared_col;
-            *((int4 *)(smem[shared_row] + shared_col)) = *((int4 *)(A + M * block_row + block_col));
+            int block_col = k_tile  * WARP_K + shared_col;
+            *((int4 *)(smem[shared_row] + shared_col)) = *((int4 *)(A + K * block_row + block_col));
         }
         // B : global to share
         for (int i = 0; i < BLOCK_COLS / BLOCK_WARPS / 8; ++i) {
             int shared_row = warp_id * BLOCK_COLS / BLOCK_WARPS + lane_id / 4 + i * 8;
             int shared_col = lane_id % 4 * 8;
             int block_col = block_tile_j * BLOCK_COLS + shared_row;
-            int block_row = k_tile * CHUNK * WARP_K + shared_col;
-            *((int4 *)(smem[BLOCK_ROWS + shared_row] + shared_col)) = *((int4 *)(B + N * block_col + block_row));
+            int block_row = k_tile * WARP_K + shared_col;
+            *((int4 *)(smem[BLOCK_ROWS + shared_row] + shared_col)) = *((int4 *)(B + K * block_col + block_row));
         }
         __syncthreads();
         for (int k = 0; k < CHUNK; ++k) {
@@ -75,14 +75,41 @@ __global__ void mmaBaseKernelTest(half *A, half *B, half *C, size_t M, size_t N,
         }
     }
     // register to shared
-
-    // shared to global
+    for (int warp_tile_m = 0; warp_tile_m < WARP_TILES_M; warp_tile_m++) {
+        for (int warp_tile_n = 0; warp_tile_n < WARP_TILES_N; warp_tile_n++) {
+            int c_row = warp_id / 2 * (WARP_M * WARP_TILES_M) + warp_tile_m * WARP_M;
+            int c_col = warp_id % 2 * (WARP_N * WARP_TILES_N) + warp_tile_n * WARP_N;
+            *((uint32_t *)(&smem[0][0] + (c_row + lane_id / 4) * SHARED_STRIDE_C + c_col) + lane_id % 4) =
+                RC[warp_tile_m][warp_tile_n][0];
+            *((uint32_t *)(&smem[0][0] + (c_row + lane_id / 4 + 8) * SHARED_STRIDE_C + c_col) + lane_id % 4) =
+                RC[warp_tile_m][warp_tile_n][1];
+        }
+    }
+    __syncthreads();
+    // C shared to global
+    for (int i = 0; i < BLOCK_ROWS / BLOCK_WARPS / 2; ++i) {
+        int shared_row = warp_id * BLOCK_ROWS / BLOCK_WARPS + lane_id / 16 + i * 2;
+        int shared_col = lane_id % 16 * 8;
+        int block_row = block_tile_i * BLOCK_ROWS + shared_row;
+        int block_col = block_tile_j * BLOCK_COLS + shared_col;
+        *((int4 *)(C + N * block_row + block_col)) =
+            *((int4 *)(&smem[0][0] + shared_row * SHARED_STRIDE_C + shared_col));
+    }
 }
 
 size_t initMmaBaseTest() {
+    int dev_id = 0;
+    HGEMM_CHECK_CUDART_ERROR(cudaGetDevice(&dev_id));
+    cudaDeviceProp dev_prop;
+    HGEMM_CHECK_CUDART_ERROR(cudaGetDeviceProperties(&dev_prop, dev_id));
     size_t ab_shared_size = (BLOCK_ROWS + BLOCK_COLS) * SHARED_STRIDE_AB * sizeof(half);
     size_t c_shared_size = BLOCK_ROWS * SHARED_STRIDE_C * sizeof(half);
-    return max(ab_shared_size, c_shared_size);
+    size_t smem_max_size = std::max(ab_shared_size, c_shared_size);
+    HLOG("smem_max_size: %.0f KBytes (%zu Bytes)", static_cast<double>(smem_max_size) / 1024, smem_max_size);
+    HGEMM_CHECK_GT(dev_prop.sharedMemPerMultiprocessor, smem_max_size);
+    HGEMM_CHECK_CUDART_ERROR(
+        cudaFuncSetAttribute(mmaBaseKernelTest, cudaFuncAttributeMaxDynamicSharedMemorySize, smem_max_size));
+    return smem_max_size;
 }
 
 void mmaBaseTest(half *A, half *B, half *C, size_t M, size_t N, size_t K) {
